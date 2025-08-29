44b8cb1cb761ef009f8989f0fb7b3677042fad20
Isaac <isaacatkinson04@gmail.com>
2025-08-29 03:46:56 +0100

    Get an input from the user and get the computers choice

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
+
+void computerDecision();
 
 int main() {
 
+    cout << "Welcome to Rock Paper Scissors!\n\n";
+
+    cout << "Choose Rock, Paper or Scissors:\n";
+
+    // Take the users choice as a string
+    string choice;
+    cin >> choice;
+
+
+}
+
+void computerDecision() {
+
+    // random seed generate for a random number
+    srand(time(0));
+
+    // generate the random number
+    const int randomNumber = rand();
+
+    // Generate the random number between 0 and 100
+    const int number = (randomNumber % 3) + 1;
+
+    if (number == 1) {
+
+        cout << "Computer: Rock";
+    } else if (number == 2) {
+
+        cout << "Computer: Paper";
+    }
+    else {
+
+        cout << "Computer: Scissors";
+    }
 
 }
